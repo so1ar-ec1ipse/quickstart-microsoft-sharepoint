ab36748ce6ac817e8c1229e306720eccdc6131bf
Yaron Shani <yaron.shani@gmail.com>
2019-04-23 16:09:01 +0300

    IESecurity issue with 2012 R2  (#11)
    * Update IE security
    
    * tabs

--- scripts/Install-SP2016PreReqs.ps1
+++ scripts/Install-SP2016PreReqs.ps1
@@ -1,7 +1,14 @@
-﻿try {
+try {
     $ErrorActionPreference = "Stop"
 
     Start-Transcript -Path c:\cfn\log\Install-SP2016PreReqs.ps1.txt -Append
+
+    # because of https://community.spiceworks.com/topic/1991530-fixed-sharepoint-2016-never-installs-pre-requisites-properly
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer -Confirm
 
     $driveLetter = Get-Volume | ?{$_.DriveType -eq 'CD-ROM'} | select -ExpandProperty DriveLetter
     if ($driveLetter.Count -gt 1) {
